74b40ac3017ecb1b0a35b321b0531bd002770350
Maxim Mikheyev <mma210977@gmail.com>
2021-01-10 15:20:21 +0700

    initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+
+        long actual = service.calculate(amount, registered);
+
+        boolean passed = expected == actual;
+
+        System.out.println(passed);
+    }
+}
